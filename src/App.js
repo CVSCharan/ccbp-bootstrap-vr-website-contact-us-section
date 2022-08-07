2c73da0253a20b9ae4e5db8eb77c7a51ed58cc39
CVSCharan <charan.cvs@gmail.com>
2022-08-07 18:14:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-7 order-md-2 pt-5">
+            <h1 className="heading">
+              Virtually Anything <br />
+              is Possible
+            </h1>
+
+            <div className="d-md-none pt-3">
+              <img
+                src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/vr-set-img.png"
+                className="img"
+                alt="img1"
+              />
+            </div>
+            <div>
+              <button className="butn btn-outline-light mt-3">
+                CONTACT US
+              </button>
+            </div>
+          </div>
+          <div className="d-none col-md-5 d-md-block order-md-1 pt-3">
+            <img
+              src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/vr-set-img.png"
+              className="img"
+              alt="img2"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
